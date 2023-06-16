b57b586a0c75a749fba5aaf61cfd347b405d6c8e
James Brian <jamesbrian1504@gmail.com>
2023-06-16 12:47:02 -0600

    4 add

--- index.js
+++ index.js
@@ -2,29 +2,36 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const userRoutes = require("./src/routes/user");
-const productRoutes = require("./src/routes/product")
-
+const productRoutes = require("./src/routes/product");
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 9000;
 
-//middleware
+// Middleware
+app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Aquí está tu API");
+});
+
 app.use("/api", userRoutes);
 app.use('/api', productRoutes);
 
-
-///routes
-app.get("/", (req, res) => {
-  res.send("Aqui esta tu api");
-});
-
-///mongodb conexion
+// MongoDB conexión
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() => console.log("Estas conectado Mongo Atlas"))
+  .then(() => console.log("Estás conectado a MongoDB Atlas"))
   .catch((error) => console.error(error));
 
 app.listen(port, () =>
-  console.log("El servido se esta ejecuentando en puerto", port)
+  console.log("El servidor se está ejecutando en el puerto", port)
 );
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const userSchema = require("../models/user");
-
 const router = express.Router();
 
 //empoid crear un usuario ruta
